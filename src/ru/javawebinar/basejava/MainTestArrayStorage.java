84613189d4245b690a09ccf5d173b36806a39591
DenisYamalov <nokiden91@gmail.com>
2024-01-17 23:29:23 +0300

    feat: implemented saving into sorted array

--- src/ru/javawebinar/basejava/MainTestArrayStorage.java
+++ src/ru/javawebinar/basejava/MainTestArrayStorage.java
@@ -9,7 +9,7 @@ import ru.javawebinar.basejava.storage.Storage;
  * Test for your ArrayStorage implementation
  */
 public class MainTestArrayStorage {
-    static final Storage ARRAY_STORAGE = new ArrayStorage();
+    static final Storage ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -6,27 +6,24 @@ import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
-    public void clear() {
-
-    }
-
-    @Override
     public void save(Resume r) {
-
+        int index = getIndex(r.getUuid());
+        System.out.println("Current index = " + index);
+        if (countResumes >= storage.length) {
+            System.out.println("Storage fulfilled, resume " + r.getUuid() + " not added");
+        } else if (index >= 0) {
+            System.out.println("Resume uuid = " + r.getUuid() + " exists");
+        } else {
+            int insertIndex = Math.abs(index) - 1;
+            System.arraycopy(storage, insertIndex + 1, storage, insertIndex + 2, countResumes);
+            storage[insertIndex] = r;
+            System.out.println("Resume added: " + r.getUuid());
+            countResumes++;
+        }
     }
 
     @Override
     public void delete(String uuid) {
-
-    }
-
-    @Override
-    public Resume[] getAll() {
-        return new Resume[0];
-    }
-
-    @Override
-    public void update(Resume resume) {
 
     }
 
