f4f6c14de999d93912e1d339a8e570dab60eb42f
DenisYamalov <nokiden91@gmail.com>
2024-01-22 22:28:39 +0300

    feat: tests added, not implemented

---
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,41 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractArrayStorageTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void update() {
+    }
+}
---
+++ test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
@@ -0,0 +1,8 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortedArrayStorageTest extends AbstractArrayStorageTest {
+}
