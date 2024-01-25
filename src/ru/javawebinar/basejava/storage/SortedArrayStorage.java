69411bed43ba5dc63146a5ce886a5543cc4867b4
DenisYamalov <nokiden91@gmail.com>
2024-01-25 23:48:14 +0300

    fix:array coppying deleteResume and insertResume corrected

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -14,13 +14,17 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected void insertResume(int index, Resume r) {
         int insertIndex = Math.abs(index) - 1;
-        System.arraycopy(storage, insertIndex + 1, storage, insertIndex + 2, countResumes);
+        System.arraycopy(storage, insertIndex, storage, insertIndex + 1, countResumes - insertIndex);
         storage[insertIndex] = r;
         System.out.println("Resume added: " + r.getUuid());
     }
 
     @Override
     protected void deleteResume(int index) {
-        System.arraycopy(storage, index + 1, storage, index, countResumes);
+        int numberMoved = countResumes - index - 1;
+        if (numberMoved > 0) {
+            System.arraycopy(storage, index + 1, storage, index, numberMoved);
+        }
     }
+
 }
