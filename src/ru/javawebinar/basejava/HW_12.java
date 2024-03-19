16c61d2c3454aedc1f68d8bc212971b9058665c9
DenisYamalov <nokiden91@gmail.com>
2024-03-19 10:55:57 +0300

    feat: implemented oddOrEven method

--- src/ru/javawebinar/basejava/HW_12.java
+++ src/ru/javawebinar/basejava/HW_12.java
@@ -2,25 +2,39 @@ package ru.javawebinar.basejava;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HW_12 {
     public static void main(String[] args) {
-        int[] firstArray = {1,2,3,3,2,3};
-        int[] secondArray = {9,8};
+        int[] firstArray = {1, 2, 3, 3, 2, 3};
+        int[] secondArray = {9, 8};
         System.out.println(minValue(firstArray));
         System.out.println(minValue(secondArray));
 
+        System.out.println();
+        System.out.println(oddOrEven(Arrays.stream(firstArray).boxed().collect(Collectors.toList())));
+
     }
-    private static int minValue(int[] values){
+
+    private static int minValue(int[] values) {
         final int[] counter = new int[1];
-        counter[0]=1;
+        counter[0] = 1;
         return Arrays.stream(values)
                 .distinct()
                 .boxed()
                 .sorted(Collections.reverseOrder())
-                .map(x->x*counter[0])
-                .peek(x->counter[0]=counter[0]*10)
+                .map(x -> x * counter[0])
+                .peek(x -> counter[0] = counter[0] * 10)
                 .reduce(Integer::sum).orElse(0);
+    }
+
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        return integers.stream()
+                .collect((Collectors.collectingAndThen(Collectors
+                                .groupingBy(t -> t % 2 == 0),
+                        booleanListMap -> booleanListMap.get(false).size() % 2 == 0
+                                ? booleanListMap.get(false)
+                                : booleanListMap.get(true))));
     }
 }
