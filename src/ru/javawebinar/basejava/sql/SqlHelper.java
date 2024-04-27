fb250577d316df04fe65c48b983ec8832253ce4c
DenisYamalov <nokiden91@gmail.com>
2024-04-27 09:36:16 +0300

    feat: moved html content to root /web directory and add resumes from db

--- src/ru/javawebinar/basejava/sql/SqlHelper.java
+++ src/ru/javawebinar/basejava/sql/SqlHelper.java
@@ -13,6 +13,11 @@ public class SqlHelper {
     private final ConnectionFactory connectionFactory;
 
     public SqlHelper(String dbUrl, String dbUser, String dbPassword) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
     }
 
--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,10 +1,15 @@
 package ru.javawebinar.basejava.web;
 
+import ru.javawebinar.basejava.Config;
+import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.storage.SqlStorage;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -13,10 +18,67 @@ public class ResumeServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
-//        response.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
 //        response.setContentType("text/html; charset=UTF-8");
 //        String name = request.getParameter("name");
 //        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
-        request.getRequestDispatcher("resumes_table.html").forward(request,response);
+
+        response.getWriter().write(getResumes());
+
+//        request.getRequestDispatcher("resumes_table.html").forward(request,response);
+    }
+
+    private String getResumes (){
+        SqlStorage sqlStorage = Config.get().getStorage();
+        List<Resume> resumeList = sqlStorage.getAllSorted();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<!DOCTYPE html>\n" +
+                                     "<html lang=\"en\">\n" +
+                                     "<head>\n" +
+                                     "    <meta charset=\"UTF-8\">\n" +
+                                     "    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n" +
+                                     "    <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n" +
+                                     "    <link href=\"https://fonts.googleapis.com/css2?family=Istok+Web:ital,wght@0,400;0,700;1,400;1,700&display=swap\"\n" +
+                                     "          rel=\"stylesheet\">\n" +
+                                     "    <style>\n" +
+                                     "        table {\n" +
+                                     "            font-family: \"Istok Web\", sans-serif;\n" +
+                                     "            border-collapse: collapse;\n" +
+                                     "            width: 100%;\n" +
+                                     "        }\n" +
+                                     "\n" +
+                                     "        td, th {\n" +
+                                     "            border: 1px solid #dddddd;\n" +
+                                     "            text-align: left;\n" +
+                                     "            padding: 8px;\n" +
+                                     "        }\n" +
+                                     "\n" +
+                                     "        tr:nth-child(even) {\n" +
+                                     "            background-color: #dddddd;\n" +
+                                     "        }\n" +
+                                     "    </style>\n" +
+                                     "    <title>Resume Table</title>\n" +
+                                     "</head>\n" +
+                                     "<body>\n" +
+                                     "<h1>Resume table</h1>\n" +
+                                     "<table>\n" +
+                                     "    <tr>\n" +
+                                     "        <th>UUID</th>\n" +
+                                     "        <th>Full name</th>\n" +
+                                     "    </tr>");
+        resumeList.forEach(r-> {
+            stringBuilder.append("<tr>\n" +
+                                         "        <td>");
+            stringBuilder.append(r.getUuid());
+            stringBuilder.append("</td>\n" +
+                                         "        <td>");
+            stringBuilder.append(r.getFullName());
+            stringBuilder.append("</td>\n" +
+                                         "    </tr>");
+        });
+        stringBuilder.append("</table>\n" +
+                                     "</body>\n" +
+                                     "</html>");
+        return stringBuilder.toString();
     }
 }
